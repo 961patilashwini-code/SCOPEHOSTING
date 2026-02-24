a9bdc2807d5bf2e9ee1d04b25fb49e293b242da1
Ashwini Patil <ashwinipatil@example.com>
2026-02-24 03:28:26 -0800

    updated files

--- hosting.js
+++ hosting.js
@@ -1,25 +1,25 @@
-// console.log(a);
-// var a = 6;
+console.log(a);
+var a = 6;
 
-// // //  var a = 6;
-// // //  Declaration is hoisted, initialization is NOT
-// // //  So console.log(a) prints undefined
+// //  var a = 6;
+// //  Declaration is hoisted, initialization is NOT
+// //  So console.log(a) prints undefined
 
 
 
-// console.log(b);
-// let b = 9;
+console.log(b);
+let b = 9;
 
-// // // let b = 9;
-// // // Variables declared with let are hoisted but in "Temporal Dead Zone (TDZ)"
-// // // Accessing them before declaration throws ReferenceError
+// // let b = 9;
+// // Variables declared with let are hoisted but in "Temporal Dead Zone (TDZ)"
+// // Accessing them before declaration throws ReferenceError
 
-// sayHello(); // Works
-// function sayHello() { 
-//     console.log("Hi"); 
-// }
+sayHello(); // Works
+function sayHello() { 
+    console.log("Hi"); 
+}
 
-// // greet1(); // Error if greet is let function expression
-// let greet1 = function() { 
-//     console.log("Hello"); 
-// }
+// greet1(); // Error if greet is let function expression
+let greet1 = function() { 
+    console.log("Hello"); 
+}
